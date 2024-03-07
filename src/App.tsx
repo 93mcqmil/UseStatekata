e40d98d5567a9405ab901b6326759c06c01d0b45
93mcqmil <mcqueen_1993@hotmail.com>
2024-03-07 15:56:40 +0100

    useState with object

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import TextComponent from "./Components/TextComponent/TextComponent";
 import "./App.css";
 import MyCounter from "./Components/Counter/Counter";
 import ArrayComponent from "./Components/ArrayComponent/ArrayComponent";
+import ObjectComponent from "./Components/ObjectComponent/ObjectComponent";
 
 function App() {
   return (
--- src/Components/ObjectComponent/ObjectComponent.tsx
+++ src/Components/ObjectComponent/ObjectComponent.tsx
@@ -1,7 +1,61 @@
+import { useState } from "react";
+import "./ObjectComponent.css";
+
 function ObjectComponent() {
+  const [form, setForm] = useState({
+    name: "",
+    age: "",
+    nationality: "",
+  });
+
+  const onChangeName = (event: { target: { value: any } }) => {
+    setForm((currentState) => {
+      return { ...currentState, name: event.target.value };
+    });
+  };
+  const onChangeAge = (event: { target: { value: any } }) => {
+    setForm((currentState) => {
+      return { ...currentState, age: event.target.value };
+    });
+  };
+  const onChangeNationality = (event: { target: { value: any } }) => {
+    setForm((currentState) => {
+      return { ...currentState, nationality: event.target.value };
+    });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    alert(JSON.stringify(form, null, 4));
+  };
   return (
-    <div>
+    <div className='Container'>
       <h1>Learn useState: Object Component</h1>
+      <br />
+      <h3>My Profile</h3>
+      <form onSubmit={onSubmit}>
+        <div>
+          <label>Name </label>
+          <input onChange={onChangeName} value={form.name} />
+        </div>
+        <div>
+          <label>Age </label>
+          <input onChange={onChangeAge} type='number' value={form.age} />
+        </div>
+        <div>
+          <label>Nationality </label>
+          <select onChange={onChangeNationality} value={form.nationality}>
+            <option value=''>--Select--</option>
+            <option value='American'>American</option>
+            <option value='Chinese'>Chinese</option>
+            <option value='Indian'>Indian</option>
+            <option value='Italian'>Italian</option>
+          </select>
+        </div>
+        <button onClick={onSubmit} className='btn-primary'>
+          Save Profile
+        </button>
+      </form>
     </div>
   );
 }
